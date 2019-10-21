b7023394c85af162a74b290471e92fb56876904c
sosandroid <ehavet@yahoo.fr>
2019-10-21 13:44:43 +0200

    Quick refactoring
    Quick refactor of this 2004 old code
    Move contants out of the code to give it fresh air
    Tested against FPDF 1.8.1

--- code39.php
+++ code39.php
@@ -1,45 +1,42 @@
- <?php
-/********************
-* FPDF class extension to draw Code39 bar codes in PDF document. Can be used for labels
-*
-* Version 1.0.2 - Oct 2019
-*
-* Release history : improvement for small labels draw by @gillouz
-*
-*********************/
+<?php
+
+/**
+ * @file code39.php
+ * @author  SOSAndroid
+ * @version 1.0.2
+ * @date 2019-10-21
+ *
+ * Release history : 
+ *	v1.0.0 : first release 2004
+ *	v1.0.1 : Roger V. form Alcatel-Lucent to cope with small size printed barcodes
+ *	v1.0.2 : Improvement for small labels draw by @gillouz
+ *
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * https://www.gnu.org/copyleft/gpl.html
+ *
+ * @section DESCRIPTION
+ *
+ * PHP class to draw Code39 bar codes in PDF files using FPDF library. 
+ * This class supports both standard and extended Code 39 barcodes. 
+ * The extended mode provides access to the full ASCII range (from 0 to 127). 
+ * The class also gives the ability to add a checksum.
+ * 
+ */
+
 require('fpdf.php');
 
-class PDF_Code39 extends FPDF {
-
-function Code39($x, $y, $code, $ext = true, $cks = false, $w = 0.4, $h = 20, $wide = true) {
-
-    //Display code
-    $this->SetFont('Arial', '', 10);
-    $this->Text($x, $y+$h+4, $code);
-
-    if($ext)
-    {
-        //Extended encoding
-        $code = $this->encode_code39_ext($code);
-    }
-    else
-    {
-        //Convert to upper case
-        $code = strtoupper($code);
-        //Check validity
-        if(!preg_match('|^[0-9A-Z. $/+%-]*$|', $code))
-            $this->Error('Invalid barcode value: '.$code);
-    }
-
-    //Compute checksum
-    if ($cks)
-        $code .= $this->checksum_code39($code);
-
-    //Add start and stop characters
-    $code = '*'.$code.'*';
-
-    //Conversion tables
-    $narrow_encoding = array (
+define('__NARROW_ENCODING', array (
         '0' => '101001101101', '1' => '110100101011', '2' => '101100101011',
         '3' => '110110010101', '4' => '101001101011', '5' => '110100110101',
         '6' => '101100110101', '7' => '101001011011', '8' => '110100101101',
@@ -54,9 +51,9 @@ function Code39($x, $y, $code, $ext = true, $cks = false, $w = 0.4, $h = 20, $wi
         'X' => '100101101011', 'Y' => '110010110101', 'Z' => '100110110101',
         '-' => '100101011011', '.' => '110010101101', ' ' => '100110101101',
         '*' => '100101101101', '$' => '100100100101', '/' => '100100101001',
-        '+' => '100101001001', '%' => '101001001001' );
-
-    $wide_encoding = array (
+        '+' => '100101001001', '%' => '101001001001' )
+	);
+define('__WIDE_ENCODING', array (
         '0' => '101000111011101', '1' => '111010001010111', '2' => '101110001010111',
         '3' => '111011100010101', '4' => '101000111010111', '5' => '111010001110101',
         '6' => '101110001110101', '7' => '101000101110111', '8' => '111010001011101',
@@ -71,44 +68,14 @@ function Code39($x, $y, $code, $ext = true, $cks = false, $w = 0.4, $h = 20, $wi
         'X' => '100010111010111', 'Y' => '111000101110101', 'Z' => '100011101110101',
         '-' => '100010101110111', '.' => '111000101011101', ' ' => '100011101011101',
         '*' => '100010111011101', '$' => '100010001000101', '/' => '100010001010001',
-        '+' => '100010100010001', '%' => '101000100010001');
-
-    $encoding = $wide ? $wide_encoding : $narrow_encoding;
-
-    //Inter-character spacing
-    $gap = ($w > 0.29) ? '00' : '0';
-
-    //Convert to bars
-    $encode = '';
-    for ($i = 0; $i< strlen($code); $i++)
-        $encode .= $encoding[$code{$i}].$gap;
-
-    //Draw bars
-    $this->draw_code39($encode, $x, $y, $w, $h);
-}
-
-function checksum_code39($code) {
-
-    //Compute the modulo 43 checksum
-
-    $chars = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-                            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-                            'W', 'X', 'Y', 'Z', '-', '.', ' ', '$', '/', '+', '%');
-    $sum = 0;
-    for ($i=0 ; $i<strlen($code); $i++) {
-        $a = array_keys($chars, $code{$i});
-        $sum += $a[0];
-    }
-    $r = $sum % 43;
-    return $chars[$r];
-}
-
-function encode_code39_ext($code) {
-
-    //Encode characters in extended mode
-
-    $encode = array(
+        '+' => '100010100010001', '%' => '101000100010001')
+	);
+define('__CHECKSUM_CHARS', array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
+		'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', '.', ' ', '$', 
+		'/', '+', '%')
+	);
+define('__EXTENDED_CHARSET',array(
         chr(0) => '%U', chr(1) => '$A', chr(2) => '$B', chr(3) => '$C',
         chr(4) => '$D', chr(5) => '$E', chr(6) => '$F', chr(7) => '$G',
         chr(8) => '$H', chr(9) => '$I', chr(10) => '$J', chr(11) => '£K',
@@ -140,33 +107,82 @@ function encode_code39_ext($code) {
         chr(112) => '+P', chr(113) => '+Q', chr(114) => '+R', chr(115) => '+S',
         chr(116) => '+T', chr(117) => '+U', chr(118) => '+V', chr(119) => '+W',
         chr(120) => '+X', chr(121) => '+Y', chr(122) => '+Z', chr(123) => '%P',
-        chr(124) => '%Q', chr(125) => '%R', chr(126) => '%S', chr(127) => '%T');
+        chr(124) => '%Q', chr(125) => '%R', chr(126) => '%S', chr(127) => '%T')
+	);
 
-    $code_ext = '';
-    for ($i = 0 ; $i<strlen($code); $i++) {
-        if (ord($code{$i}) > 127)
-            $this->Error('Invalid character: '.$code{$i});
-        $code_ext .= $encode[$code{$i}];
-    }
-    return $code_ext;
-}
 
-function draw_code39($code, $x, $y, $w, $h) { 
-
-	//Draw bars 
-	for($i=0; $i<strlen($code); $i++) { 
-		if($code[$i] == '1') { 
-			// set color as black
-			$this->SetFillColor(0,0,0);
-		} 
-		else { 
-			// set color as white
-			$this->SetFillColor(255,255,255); 
+class PDF_Code39 extends FPDF {
+	
+	function Code39($x, $y, $code, $ext = true, $cks = false, $w = 0.4, $h = 20, $wide = true) {
+		//Display code
+		$this->SetFont('Arial', '', 10);
+		$this->Text($x, $y+$h+4, $code);
+		if($ext)
+		{
+			//Extended encoding
+			$code = $this->encode_code39_ext($code);
 		}
-		$this->Rect($x+$i*$w, $y, $w , $h, 'F');
-	} 
-}
+		else
+		{
+			//Convert to upper case
+			$code = strtoupper($code);
+			//Check validity
+			if(!preg_match('|^[0-9A-Z. $/+%-]*$|', $code))
+				$this->Error('Invalid barcode value: '.$code);
+		}
+		//Compute checksum
+		if ($cks)
+			$code .= $this->checksum_code39($code);
+		//Add start and stop characters
+		$code = '*'.$code.'*';
 
+		$encoding = $wide ? __WIDE_ENCODING : __NARROW_ENCODING;
+		//Inter-character spacing
+		$gap = ($w > 0.29) ? '00' : '0';
+		//Convert to bars
+		$encode = '';
+		for ($i = 0; $i< strlen($code); $i++)
+			$encode .= $encoding[$code{$i}].$gap;
+		//Draw bars
+		$this->draw_code39($encode, $x, $y, $w, $h);
+	}
+	
+	function checksum_code39($code) {
+		//Compute the modulo 43 checksum
+		$sum = 0;
+		for ($i=0 ; $i<strlen($code); $i++) {
+			$a = array_keys(__CHECKSUM_CHARS, $code{$i});
+			$sum += $a[0];
+		}
+		$r = $sum % 43;
+		return __CHECKSUM_CHARS[$r];
+	}
+	
+	function encode_code39_ext($code) {
+		//Encode characters in extended mode
+		$code_ext = '';
+		for ($i = 0 ; $i<strlen($code); $i++) {
+			if (ord($code{$i}) > 127)
+				$this->Error('Invalid character: '.$code{$i});
+			$code_ext .= __EXTENDED_CHARSET[$code{$i}];
+		}
+		return $code_ext;
+	}
+	
+	function draw_code39($code, $x, $y, $w, $h) { 
+		//Draw bars 
+		for($i=0; $i<strlen($code); $i++) { 
+			if($code[$i] == '1') { 
+				// set color as black
+				$this->SetFillColor(0,0,0);
+			} 
+			else { 
+				// set color as white
+				$this->SetFillColor(255,255,255); 
+			}
+			$this->Rect($x+$i*$w, $y, $w , $h, 'F');
+		} 
+	}
 //end class
 }
 ?> 
